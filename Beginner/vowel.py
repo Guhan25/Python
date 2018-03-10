fd4f699673f56d974d1afca2833e86255adf0d33
Guhan25 <37235893+Guhan25@users.noreply.github.com>
2018-03-10 14:56:11 +0530

    Update vowel.py

--- Beginner/vowel.py
+++ Beginner/vowel.py
@@ -1,5 +1,5 @@
 v=raw_input()
 if(v=='a'or v=='A'or v=='e'or v=='E' or v=='i'or v=='I'or v=='o'or v=='O'or v=='u'or v=='U'):
-  print(v,'is vowel')
+  print(v+' is vowel')
 else:
-  print(v,'is consonant')
+  print(v+' is consonant')
