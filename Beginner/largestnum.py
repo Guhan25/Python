f93dbe7b1afe9dadd3eb734a62e95523bba68e23
Guhan25 <37235893+Guhan25@users.noreply.github.com>
2018-03-10 15:42:40 +0530

    Update largestnum.py

--- Beginner/largestnum.py
+++ Beginner/largestnum.py
@@ -1,9 +1,9 @@
 a=raw_input()
 b=raw_input()
 c=raw_input()
-if((a>b)and(a>c)):
+if((a>b)or (a>c)):
  print("a is greater")
-elif((b>a)and(b>c)):
+elif((b>a)or(b>c)):
   print("b is greater ")
 else:
   print("c is greater ")
