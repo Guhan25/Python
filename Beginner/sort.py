922b9f90e37c79b7c67c3fcbc961c6c334d94d66
Guhan25 <37235893+Guhan25@users.noreply.github.com>
2019-02-05 14:48:42 +0530

    Update sort.py

--- Beginner/sort.py
+++ Beginner/sort.py
@@ -1,7 +1,8 @@
-a=[]
-n=int(input("Enter number of elements:"))
+o=[]
+n=int(input())
 for i in range(1,n+1):
     b=input("")
-    a.append(b)
-a.sort()
-print(a)
+    o.append(b)
+o.sort()
+print(o)
+
